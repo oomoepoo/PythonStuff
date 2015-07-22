104f27e23b0a47f2f589cb4bd28d330d608fd3ca
Jan <moep_2007@yahoo.de>
2015-07-22 19:52:10 +0200

    Now with added Python3 awesomeness \ :D /

---
+++ pyjs-zeug/autoalienationpyjs.py
@@ -0,0 +1,24 @@
+# Erstmals in Python3, hooray!
+# Aufgabe: Alienierungsdateien einlesen, alinierung darstellen und in html ausgeben
+# benötigt lingpy
+# außerdem
+
+
+import os
+from lingpy import *
+
+directory = os.path.normpath("./msa")
+html_string = "<html><header></header><body></body></html>"
+
+if not (os.path.exists(directory)):
+	print ("Couldn't find msa-directory, please put in the same folder you run this script from!"
+
+for root, dirs, files in os.walk(directory):
+	for f in files:
+		
+		
+		
+		
+			
+	
+
